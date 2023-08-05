0b19f6674bf8ee72a59a00a180ebda10e556ab62
Atromat <atromatok@gmail.com>
2023-08-05 21:24:28 +0200

    Add more endpoints and more buttons

--- index.js
+++ index.js
@@ -5,12 +5,105 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const dataRoute = "users.json";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/pub', express.static(path.join(__dirname, 'client', 'public')));
 
+app.post('/users/:userId', (req, res) => {
+    fs.readFile(dataRoute, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const { users } = JSON.parse(data);
+        const lastUser = users[users.length - 1];
+        const newUser = {
+            name: req.body.name,
+            id: lastUser.id + 1
+        }
+        users.push(newUser);
+
+        fs.writeFile(dataRoute, JSON.stringify({ users }), (err) => {
+            if (err) throw err;
+        });
+
+        res.send({ state: "DONE" });
+    });
+});
+
+app.delete('/users/:userId', (req, res) => {
+    fs.readFile(dataRoute, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const { users } = JSON.parse(data);
+        const userId = parseInt(req.params.userId);
+        const user = users.find(user => user.id === userId);
+
+        if (user) {
+
+            fs.writeFile(dataRoute, JSON.stringify({ users: users.filter((x) => x !== user) }), (err) => {
+                if (err) throw err;
+            });
+
+            res.send({ state: "DONE" });
+        } else {
+            res.status(404).send({ state: 'User not found' });
+        }
+    });
+});
+
+app.put('/users/:userId', (req, res) => {
+    fs.readFile(dataRoute, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const { users } = JSON.parse(data);
+        const userId = parseInt(req.params.userId);
+        const user = users.find((user) => user.id === userId);
+
+        if (user) {
+            user.name = req.body.name;
+            user.id = req.body.id;
+
+            fs.writeFile(dataRoute, JSON.stringify({ users }), (err) => {
+                if (err) throw err;
+            });
+
+            res.send({ state: "DONE" });
+        } else {
+            res.status(404).send({ state: 'User not found' });
+        }
+    });
+});
+
+app.patch('/users/:userId', (req, res) => {
+    fs.readFile(dataRoute, 'utf8', (err, data) => {
+        if (err) throw err;
+
+        const { users } = JSON.parse(data);
+        const userId = parseInt(req.params.userId);
+        const user = users.find(user => user.id === userId);
+
+        if (user) {
+            user.name = req.body.name;
+
+            fs.writeFile(dataRoute, JSON.stringify({ users }), (err) => {
+                if (err) throw err;
+            });
+
+            res.send({ state: "DONE" });
+        } else {
+            res.status(404).send({ state: 'User not found' });
+        }
+    });
+});
+
 app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.get('/edit', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'index.html'));
 });
 
